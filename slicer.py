5a4b9d76261d77204042b58d5654eac0d144b50b
ameijers3 <arturmeiers@gmail.com>
2022-09-29 11:59:48 +0300

    text

---
+++ slicer.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    my_string = 'y3tds23o12345dujderpvpasdtgxaefs3gss5432fjs24fsswe098765d'
+    current_slice = my_string.replace('3tds23', '')
+    current_slicea = current_slice.replace('12345d', '')
+    current_sliceb = current_slicea.replace('jderpv', '')
+    current_slicec = current_sliceb.replace('dtgxaefs3g', '')
+    current_sliced = current_slicec.replace('s5432fjs24f', '')
+    current_slicee = current_sliced.replace('ssw', '')
+    current_slicef = current_slicee.replace('098765', '')
+    print(current_slicef)
