26e25283240a16f062d2deb650627e5d5b7878e6
ameijers3 <arturmeiers@gmail.com>
2022-12-14 15:42:37 +0800

    text

---
+++ rinka_laukums.py
@@ -0,0 +1,4 @@
+a = input("Введите радиус круга: ")
+otvet = float(a) ** 2
+otvet2 = float(otvet) * 3.14
+print("Площадь круга: " + (str(otvet2)))
---
+++ temp.py
@@ -0,0 +1,4 @@
+a = input("Введите температуру в цельсиях что бы получить фаренгейты: ")
+otvet = float(a) * float(9/5)
+otvet2 = 32 + float(otvet)
+print("Ваш ответ: " + (str(otvet2)))
