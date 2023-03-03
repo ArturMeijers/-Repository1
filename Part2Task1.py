41cf150773006efb6969f6050ca2de5e98817dca
ameijers3 <arturmeiers@gmail.com>
2023-03-03 09:45:12 +0200

    text

---
+++ Part2Task1.py
@@ -0,0 +1,3 @@
+import Part2Task1_module
+a = input("Введите число: ")
+print(Part2Task1_module.funkcija(a))
---
+++ Part2Task1_module.py
@@ -0,0 +1,24 @@
+from datetime import date, timedelta
+def funkcija(a):
+    today = date.today()
+    return today + timedelta(days=int(a))
+
+
+
+
+
+
+# a = input("Введите число: ")
+# today = date.today()
+# print(today + timedelta(days=int(a)))
+
+
+
+#
+# import datetime
+# from datetime import date
+#
+# def funkcija():
+#     current_date = datetime.date.today()
+#     return current_date
+
