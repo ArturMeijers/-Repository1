8c5c4a2cdf6b18dacbf1871deb3ee576ba686e4d
ameijers3 <arturmeiers@gmail.com>
2023-03-29 09:23:07 +0300

    text

--- adwd.py
+++ adwd.py
@@ -1,5 +1,3 @@
-# how to make your favorite food entered directly into the dictionary without using a list
-
 lietotaja_info = {}
 
 lietotaja_vards = input("Ievadi savu vārdu: ")
@@ -8,23 +6,18 @@ lietotaja_info['vards'] = lietotaja_vards
 lietotaja_uzvards = input("Ievadi savu uzvārdu: ")
 lietotaja_info['uzvards'] = lietotaja_uzvards
 
-milietais_ediens = []
+milietais_ediens = ""
 while True:
     ediena_nosaukums = input("Ievadi savu mīļāko ēdienu (ja vēlies pārtraukt, raksti 'stop'): ")
     if ediena_nosaukums == "stop":
         break
     else:
-        
+        milietais_ediens += ediena_nosaukums + ", "
 
-milietais_edien = input("Ievadi savu mīļāko ēdienu: ")
-lietotaja_info['milakais_ediens'] = milietais_edien
-
-
+milietais_ediens = milietais_ediens.rstrip(", ")
 lietotaja_info['milakie_edieni'] = milietais_ediens
 
 print("Lietotāja informācija:")
 print("Vārds: " + lietotaja_info['vards'])
 print("Uzvārds: " + lietotaja_info['uzvards'])
-# print("Mīļākie ēdieni: " + lietotaja_info['milakie_edieni'])
-print("Mīļākie ēdieni: " + ', '.join(lietotaja_info['milakie_edieni']))
-print(lietotaja_info)
+print("Mīļākie ēdieni: " + lietotaja_info['milakie_edieni'])
