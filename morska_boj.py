31dfa6787ea59d7f809c4c20b79471dcef0d2ed1
ameijers3 <arturmeiers@gmail.com>
2023-05-20 09:38:57 +0300

    text

--- morska_boj.py
+++ morska_boj.py
@@ -1,43 +1,13 @@
 import random
 
-class Pirate:
-    def __init__(self):
-        self.ability_available = True
-
-    def use_ability(self, ships, x, y):
-        self.ability_available = False
-        for ship in ships:
-            if ship[0] <= x < ship[0] + ship[3] and ship[1] <= y < ship[1] + ship[3]:
-                ships.remove(ship)
-                return True
-        return False
-
-    def is_ability_available(self):
-        return self.ability_available
-
-class Fisherman:
-    def __init__(self):
-        self.ability_available = True
-
-    def use_ability(self, user_field):
-        self.ability_available = False
-        num_revealed = 0
-        while num_revealed < 5:
-            x = random.randint(0, len(user_field[0]) - 1)
-            y = random.randint(0, len(user_field) - 1)
-            if user_field[y][x] == '~':
-                user_field[y][x] = '?'
-                num_revealed += 1
-
-    def is_ability_available(self):
-        return self.ability_available
-
 # Запрос размера поля у пользователя
 while True:
     try:
         field_size = int(input("Введите размер поля (одно число): "))
         if field_size <= 0:
             print("Размер поля должен быть положительным числом.")
+        elif field_size > 11:
+            print("Размер поля не может быть больше 11.")
         else:
             break
     except ValueError:
@@ -49,26 +19,12 @@ while True:
         num_ships = int(input("Введите количество кораблей (одно число): "))
         if num_ships <= 0:
             print("Количество кораблей должно быть положительным числом.")
-        elif num_ships > field_size:
+        elif num_ships > field_size * field_size:
             print("Количество кораблей не может быть больше размера поля.")
         else:
             break
     except ValueError:
         print("Ошибка ввода. Попробуйте снова.")
-
-# Выбор персонажа
-print("Выберите персонажа:")
-print("1. Пират (может выпить ягеря и потопить задетый корабль)")
-print("2. Рыбак (может поймать рыбку и открыть 5 неизвестных мест)")
-character_choice = input("Введите номер персонажа: ")
-
-if character_choice == '1':
-    character = Pirate()
-elif character_choice == '2':
-    character = Fisherman()
-else:
-    print("Неверный выбор персонажа. Будет выбран персонаж по умолчанию.")
-    character = Pirate()
 
 # Создаем пустое поле для пользователя
 user_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
@@ -80,31 +36,46 @@ ship_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
 ships = []
 for _ in range(num_ships):
     while True:
-        x = random.randint(0, field_size - 1)
-        y = random.randint(0, field_size - 1)
+        length = random.randint(1, min(4, field_size))
         is_vertical = random.choice([True, False])
-        length = random.randint(1, 4)
+
         if is_vertical:
-            if y + length <= field_size and all(ship_field[y + i][x] == '~' for i in range(length)):
+            x = random.randint(0, field_size - 1)
+            y = random.randint(0, field_size - length)
+
+            # Проверяем, что все клетки для размещения корабля свободны
+            if all(ship_field[y+i][x] == '~' for i in range(length)):
                 for i in range(length):
-                    ship_field[y + i][x] = 'S'
+                    ship_field[y+i][x] = 'S'
                 ships.append((x, y, is_vertical, length))
                 break
         else:
-            if x + length <= field_size and all(ship_field[y][x + i] == '~' for i in range(length)):
+            x = random.randint(0, field_size - length)
+            y = random.randint(0, field_size - 1)
+
+            # Проверяем, что все клетки для размещения корабля свободны
+            if all(ship_field[y][x+i] == '~' for i in range(length)):
                 for i in range(length):
-                    ship_field[y][x + i] = 'S'
+                    ship_field[y][x+i] = 'S'
                 ships.append((x, y, is_vertical, length))
                 break
 
-# Функция активации способности персонажа
-def activate_ability():
-    if isinstance(character, Pirate):
-        if character.use_ability(ships, x, y):
-            user_field[y][x] = 'S'
-            print("Персонаж потопил корабль!")
-    elif isinstance(character, Fisherman):
-        character.use_ability(user_field)
+# Функция для показа всех кораблей и завершения игры
+def surrender():
+    print("Вы сдались! Вот все корабли:")
+    for ship in ships:
+        x, y, is_vertical, length = ship
+        if is_vertical:
+            for i in range(length):
+                user_field[y+i][x] = 'S'
+        else:
+            for i in range(length):
+                user_field[y][x+i] = 'S'
+
+    # Выводим поле с кораблями
+    print("   " + ' '.join(str(i) for i in range(field_size)))
+    for i, row in enumerate(user_field):
+        print(f"{i}  {' '.join(row)}")
 
 # Игровой цикл
 while True:
@@ -113,51 +84,29 @@ while True:
     for i, row in enumerate(user_field):
         print(f"{i}  {' '.join(row)}")
 
-    # Пользователь вводит координаты выстрела или команду "сдаюсь" или активирует способность
+    # Пользователь вводит координаты выстрела или команду "сдаюсь"
     while True:
-        user_input = input("Введите координату X или команду 'сдаюсь' или 'способность': ")
+        user_input = input("Введите координаты выстрела (X Y) или 'сдаюсь': ")
         if user_input.lower() == 'сдаюсь':
-            for ship in ships:
-                x, y, _, _ = ship
-                user_field[y][x] = 'S'
-            print("Вы сдались. Все корабли:")
-            for row in user_field:
-                print(' '.join(row))
+            surrender()
             exit()
-        elif user_input.lower() == 'способность':
-            if character.is_ability_available():
-                activate_ability()
-                print("Вы активировали способность!")
-                break
-            else:
-                print("Способность уже была использована. Введите координату X.")
-        else:
-            try:
-                x = int(user_input)
-                y = int(input("Введите координату Y: "))
-                if x < 0 or x >= field_size or y < 0 or y >= field_size:
-                    raise ValueError
-                break
-            except ValueError:
-                print("Неверные координаты. Попробуйте снова.")
+        try:
+            x, y = map(int, user_input.split())
+            if x < 0 or x >= field_size or y < 0 or y >= field_size:
+                raise ValueError
+            break
+        except ValueError:
+            print("Неверные координаты. Попробуйте снова.")
 
     # Проверяем, попал ли пользователь в корабль, и обновляем поле пользователя
     if any(ship[0] <= x < ship[0] + ship[3] and ship[1] <= y < ship[1] + ship[3] for ship in ships):
         print("Попадание!")
-        if isinstance(character, Pirate):
-            activate_ability()
-            print("Персонаж выполнил способность!")
-        else:
-            for ship in ships:
-                if ship[0] <= x < ship[0] + ship[3] and ship[1] <= y < ship[1] + ship[3]:
-                    user_field[ship[1]:ship[1] + ship[3]][ship[0]] = ['S'] * ship[3]
-                    print("Корабль уничтожен!")
-                    break
+        user_field[y][x] = 'X'
     else:
-        print("Промах!")
+        print("Мимо!")
         user_field[y][x] = 'O'
 
-    # Проверяем, остались ли еще корабли на поле
-    if not ships:
+    # Проверяем условие победы
+    if all(user_field[y][x] != '~' for y in range(field_size) for x in range(field_size)):
         print("Поздравляем! Вы потопили все корабли!")
-        exit()
+        break
--- morskoj_boj2.py
+++ morskoj_boj2.py
@@ -1,5 +1,23 @@
 import random
 
+# Функция выбора персонажа и его способности
+def choose_character():
+    while True:
+        character = input("Выберите персонажа (пират или рыбак): ").lower()
+        if character == "пират":
+            ability = "выпить ягеря"
+            break
+        elif character == "рыбак":
+            ability = "поймать рыбку"
+            break
+        else:
+            print("Некорректный выбор персонажа. Попробуйте снова.")
+    return character, ability
+
+# Запрос выбора персонажей у пользователей
+player1_character, player1_ability = choose_character()
+player2_character, player2_ability = choose_character()
+
 # Запрос размера поля у пользователя
 while True:
     try:
@@ -24,8 +42,9 @@ while True:
     except ValueError:
         print("Ошибка ввода. Попробуйте снова.")
 
-# Создаем пустое поле для пользователя
-user_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
+# Создаем пустое поле для каждого игрока
+player1_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
+player2_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
 
 # Создаем поле для кораблей
 ship_field = [['~' for _ in range(field_size)] for _ in range(field_size)]
@@ -54,6 +73,10 @@ for _ in range(num_ships):
                 ships.append((x, y, is_vertical, length))
                 break
 
+# Переменные для отслеживания активации способностей
+player1_ability_used = False
+player2_ability_used = False
+
 # Функция для показа всех кораблей и завершения игры
 def surrender():
     print("Вы сдались! Вот все корабли:")
@@ -61,21 +84,45 @@ def surrender():
         x, y, is_vertical, length = ship
         if is_vertical:
             for i in range(length):
-                user_field[y+i][x] = 'S'
+                player1_field[y+i][x] = 'S'
+                player2_field[y+i][x] = 'S'
         else:
             for i in range(length):
-                user_field[y][x+i] = 'S'
+                player1_field[y][x+i] = 'S'
+                player2_field[y][x+i] = 'S'
 
-    # Выводим поле с кораблями
+    # Выводим поле с кораблями для каждого игрока
+    print("Поле игрока 1:")
     print("   " + ' '.join(str(i) for i in range(field_size)))
-    for i, row in enumerate(user_field):
+    for i, row in enumerate(player1_field):
+        print(f"{i}  {' '.join(row)}")
+
+    print("Поле игрока 2:")
+    print("   " + ' '.join(str(i) for i in range(field_size)))
+    for i, row in enumerate(player2_field):
         print(f"{i}  {' '.join(row)}")
 
 # Игровой цикл
+current_player = 1
 while True:
-    # Выводим поле пользователя с номерами строк
+    # Определяем текущего игрока и его поле
+    if current_player == 1:
+        current_field = player1_field
+        opponent_field = player2_field
+        current_ability_used = player1_ability_used
+        current_character = player1_character
+        current_ability = player1_ability
+    else:
+        current_field = player2_field
+        opponent_field = player1_field
+        current_ability_used = player2_ability_used
+        current_character = player2_character
+        current_ability = player2_ability
+
+    # Выводим поле текущего игрока с номерами строк
+    print(f"Поле игрока {current_player}:")
     print("   " + ' '.join(str(i) for i in range(field_size)))
-    for i, row in enumerate(user_field):
+    for i, row in enumerate(current_field):
         print(f"{i}  {' '.join(row)}")
 
     # Пользователь вводит координаты выстрела или команду "сдаюсь"
@@ -92,15 +139,54 @@ while True:
         except ValueError:
             print("Неверные координаты. Попробуйте снова.")
 
-    # Проверяем, попал ли пользователь в корабль, и обновляем поле пользователя
+    # Проверяем, попал ли текущий игрок в корабль оппонента, и обновляем поля
     if any(ship[0] <= x < ship[0] + ship[3] and ship[1] <= y < ship[1] + ship[3] for ship in ships):
         print("Попадание!")
-        user_field[y][x] = 'X'
+        current_field[y][x] = 'X'
+        if current_ability_used:
+            for ship in ships:
+                ship_x, ship_y, is_vertical, length = ship
+                if ship_x <= x < ship_x + length and ship_y <= y < ship_y + length:
+                    if is_vertical:
+                        for i in range(length):
+                            current_field[ship_y+i][ship_x] = 'X'
+                            opponent_field[ship_y+i][ship_x] = 'X'
+                    else:
+                        for i in range(length):
+                            current_field[ship_y][ship_x+i] = 'X'
+                            opponent_field[ship_y][ship_x+i] = 'X'
+                    print("Корабль полностью уничтожен!")
+                    break
     else:
         print("Мимо!")
-        user_field[y][x] = 'O'
+        current_field[y][x] = 'O'
+        opponent_field[y][x] = 'O'
 
     # Проверяем условие победы
-    if all(user_field[y][x] != '~' for y in range(field_size) for x in range(field_size)):
-        print("Поздравляем! Вы потопили все корабли!")
+    if all(current_field[y][x] != '~' for y in range(field_size) for x in range(field_size)):
+        print(f"Игрок {current_player} победил! Все корабли потоплены!")
         break
+
+    # Активируем способность текущего игрока, если она не была использована
+    if not current_ability_used:
+        if current_character == "пират":
+            pirate_input = input("Активировать способность 'выпить ягеря'? (да/нет): ")
+            if pirate_input.lower() == "да":
+                current_ability_used = True
+                print("Активирована способность пирата!")
+        elif current_character == "рыбак":
+            fisherman_input = input("Активировать способность 'поймать рыбку'? (да/нет): ")
+            if fisherman_input.lower() == "да":
+                current_ability_used = True
+                print("Активирована способность рыбака!")
+                fish_revealed = 0
+                while fish_revealed < 5:
+                    x = random.randint(0, field_size - 1)
+                    y = random.randint(0, field_size - 1)
+                    if current_field[y][x] == '~':
+                        current_field[y][x] = 'F'
+                        opponent_field[y][x] = 'F'
+                        fish_revealed += 1
+
+    # Переключаемся на следующего игрока
+    current_player = 2 if current_player == 1 else 1
